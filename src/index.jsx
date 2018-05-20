6c70bee553c5376ee7879841172d931541e269eb
Alejandro <alejovp@gmail.com>
2018-05-20 13:09:04 +0200

    Youtube call service added on most parent component

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCsRxUXM6-jp930ID7WkQ9Y1RNIEJ2oaao';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }
+
+    YTSearch(
+      { key: API_KEY, term: 'surfboards' }, 
+      (resVideos) => {
+        this.setState({ videos: resVideos });
+      }
+    );
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
